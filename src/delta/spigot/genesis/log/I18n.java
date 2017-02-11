456f376e9109a90540556c38092d523fbe85fd60
4513 <sirmichal.4513@gmail.com>
2017-02-11 13:44:27 +0100

    Add files via upload

--- src/delta/spigot/genesis/log/I18n.java
+++ src/delta/spigot/genesis/log/I18n.java
@@ -1,1 +1,23 @@
-s
+package delta.spigot.genesis.log;
+
+import delta.spigot.genesis.entity.PlayerCharacter;
+
+public class I18n
+{
+	public I18n()
+	{
+		
+	}
+	
+	public static void sendMessage(PlayerCharacter target, String message) {
+		target.getPlayer().sendMessage(message);
+	}
+	
+	public static void tl(String message) {
+		
+	}
+	
+	public static void tl(String message, Object obj) {
+		
+	}
+}
