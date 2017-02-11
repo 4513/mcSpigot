6b8d21d605118d3131f89f18e49c89c733d493ad
adyhnat <adyhnat@gmail.com>
2017-02-11 22:45:33 +0100

    Update PlayerMoveListener.java

--- src/delta/spigot/genesis/event/PlayerMoveListener.java
+++ src/delta/spigot/genesis/event/PlayerMoveListener.java
@@ -12,6 +12,8 @@ import delta.spigot.genesis.Genesis;
 
 public class PlayerMoveListener extends Listener
 {
+
+	
 	public PlayerMoveListener(GenesisPlugin plugin) {
 		super(plugin);
 	}
@@ -21,13 +23,18 @@ public class PlayerMoveListener extends Listener
 		
 		User player = new User(event.getPlayer());
 		
-		int countJumps = player.getConfig().getInt(player.getName() + ".Jumps");
+		int countJumps = player.getConfig().getInt(player.getName() + ".JumpsTMP");
 		
 		if(event.getFrom().getY()<event.getTo().getY()){
 			countJumps = countJumps+1;
 		}
 		
-		player.getConfig().set(player.getName() + ".Jumps", countJumps);
+		player.getConfig().set(player.getName() + ".JumpsTMP", countJumps);
+		
+		if(player.getConfig().getInt(player.getName() + ".JumpsTMP")%6 == 0) {
+			player.getConfig().set(player.getName() + ".JumpsTMP", 0);
+			player.getConfig().set(player.getName() + ".Jumps", player.getConfig().getInt(player.getName() + ".Jumps")+1);
+		}
 		
 		try {
     			player.getConfig().save(Genesis.usersFile);
