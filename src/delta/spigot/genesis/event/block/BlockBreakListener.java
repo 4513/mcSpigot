a3083b22f99d5b6d678593b66ea59b403ee38b62
adyhnat <adyhnat@gmail.com>
2017-02-13 21:46:31 +0100

    Update BlockBreakListener.java

--- src/delta/spigot/genesis/event/block/BlockBreakListener.java
+++ src/delta/spigot/genesis/event/block/BlockBreakListener.java
@@ -21,6 +21,25 @@ public class BlockBreakListener extends Listener
 		if (!player.isAuthorized("genesis.modifyworld.break"))
 			event.setCancelled(true);
 		
+		int x = event.getBlock().getX();
+                int y = event.getBlock().getY();
+                int z = event.getBlock().getZ();
+                
+                Material type = event.getBlock().getType();
+    
+                DateFormat format = new SimpleDateFormat("dd.MM.yyyy - hh.mm.ss");
+                Date date = new Date();
+                
+                String currentTime = format.format(date).toString();
+
+                YamlConfiguration file = YamlConfiguration.loadConfiguration(Genesis.rbFile);
+                file.set(x+","+y+","+z+"."+currentTime+"."+type, "removedBlock");
+
+                try {
+                        file.getConfig().save(Genesis.usersFile);
+                } catch (IOException e) {
+                        e.printStackTrace();
+                }
 		
 	}
 }
