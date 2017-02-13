97f9011be8b40bdf21dd697df80a1f8fb4fa9528
4513 <sirmichal.4513@gmail.com>
2017-02-13 21:09:01 +0100

    Add files via upload

--- src/delta/spigot/genesis/event/block/BlockBreakListener.java
+++ src/delta/spigot/genesis/event/block/BlockBreakListener.java
@@ -2,23 +2,23 @@ package delta.spigot.genesis.event.block;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
-import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.block.BlockBreakEvent;
 
 import delta.spigot.genesis.GenesisPlugin;
 import delta.spigot.genesis.entity.User;
 import delta.spigot.genesis.event.Listener;
 
-public class BlockPlaceListener extends Listener
+public class BlockBreakListener extends Listener
 {
-	public BlockPlaceListener(GenesisPlugin plugin) {
+	public BlockBreakListener(GenesisPlugin plugin) {
 		super(plugin);
 	}
-	
+
 	@EventHandler (priority = EventPriority.NORMAL)
-	public void onBlockPlace(BlockPlaceEvent event) {
+	public void onBlockBreak(BlockBreakEvent event) {
 		User player = new User(event.getPlayer());
 		
-		if (!player.isAuthorized("genesis.modifyworld.place"))
+		if (!player.isAuthorized("genesis.modifyworld.break"))
 			event.setCancelled(true);
 		
 		
