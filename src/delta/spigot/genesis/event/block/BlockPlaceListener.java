8443deb16f48a0538e1bab1b33ca71034e14ef16
adyhnat <adyhnat@gmail.com>
2017-02-13 21:42:48 +0100

    Update BlockPlaceListener.java

--- src/delta/spigot/genesis/event/block/BlockPlaceListener.java
+++ src/delta/spigot/genesis/event/block/BlockPlaceListener.java
@@ -21,6 +21,28 @@ public class BlockPlaceListener extends Listener
 		if (!player.isAuthorized("genesis.modifyworld.place"))
 			event.setCancelled(true);
 		
+		int x = event.getBlock().getX();
+		int y = event.getBlock().getY();
+		int z = event.getBlock().getZ();
+		
+		Material type = event.getBlock().getType();
+		
+		
+		DateFormat format = new SimpleDateFormat("dd.MM.yyyy - hh.mm.ss");
+		Date date = new Date();
+		
+		String currentTime = format.format(date).toString();
+		
+		YamlConfiguration file = YamlConfiguration.loadConfiguration(Genesis.rbFile);
+		file.set(x+","+y+","+z+"."+currentTime+"."+type, "placedBlock");
+		
+		try {
+      			file.getConfig().save(Genesis.usersFile);
+    		} catch (IOException e) {
+       			e.printStackTrace();
+  		}
+		
+		
 		
 	}
 }
