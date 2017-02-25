0ceeddcb83b0736d4775a60f33212a75892f505f
adyhnat <adyhnat@gmail.com>
2017-02-25 23:14:18 +0100

    Update User.java

--- src/delta/spigot/genesis/entity/User.java
+++ src/delta/spigot/genesis/entity/User.java
@@ -1,9 +1,11 @@
 package delta.spigot.genesis.entity;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bukkit.GameMode;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 
@@ -12,10 +14,13 @@ import delta.spigot.genesis.command.ICommandSource;
 
 public class User extends PlayerCharacter implements ICommandSource
 {
-	private YamlConfiguration confFile = YamlConfiguration.loadConfiguration(Genesis.usersFile);
+	public File userFile = new File("plugins" + File.separator + "Genesis" + File.separator + "users" + File.separator + this.getName(true) + ".yml");
+	private YamlConfiguration confFile = YamlConfiguration.loadConfiguration(userFile);
+	public static List<User> users;
 	
 	public User(Player player) {
 		super(player);
+		//users.add(this);
 	}
 
 	public static List<User> listOfUsers;
@@ -24,51 +29,79 @@ public class User extends PlayerCharacter implements ICommandSource
 		delta.spigot.genesis.log.I18n.sendMessage(this, message);
 	}
 	
-	public void register() {
-		YamlConfiguration userFile = YamlConfiguration.loadConfiguration(Genesis.usersFile);
+	public void register() throws IOException {
 		if (isRegistered()) {
-			userFile.set(this.getName(true) + ".LastPlayedIP", this.getIPAddress());
-			userFile.set(this.getName(true) + ".Speed.Walk", (Math.sqrt(userFile.getDouble(this.getName(true) + ".SpeedTime"))/35000) + 0.10);
-			if (userFile.getBoolean(this.getName(true) + ".Speed.Allow")) {
-				this.setWalkSpeed((float) userFile.getDouble(this.getName(true) + ".Speed.Walk")); 
-			}
-			try {
-				userFile.save(Genesis.usersFile);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+			getConfig().set("Location.LastIP", getIPAddress());
+			getConfig().set(this.getName(true) + ".Speed.Walk", ((Math.sqrt(getConfig().getDouble(this.getName(true) + ".SpeedTime"))/45000) + Genesis.configFile.getDouble("UC.Move.Speed.Walk")) <= 0.99 ? ((Math.sqrt(getConfig().getDouble(this.getName(true) + ".SpeedTime"))/45000) + Genesis.configFile.getDouble("UC.Move.Speed.Walk")) : 0.99);
+			if (getConfig().getBoolean("Move.Speed.Allow"))
+				setWalkSpeed((float) getConfig().getDouble("Move.Speed.Walk"));
 		} else {
-			userFile.set(this.getName(true) + ".IP", this.getIPAddress());
-			userFile.set(this.getName(true) + ".LastPlayedIP", this.getIPAddress());
-			userFile.set(this.getName(true) + ".GameMode", false);
-			userFile.set(this.getName(true) + ".FlyAllow", false);
-			userFile.set(this.getName(true) + ".Speed.Allow", true);
-			userFile.set(this.getName(true) + ".Speed.Walk", 0.1);
-			userFile.set(this.getName(true) + ".SpeedTime", 0);
-			userFile.set(this.getName(true) + ".Jumps", 0);
-			userFile.set(this.getName(true) + ".JumpsTMP", 1);
-			userFile.set(this.getName(true) + ".Energy", 0);
-			try {
-				userFile.save(Genesis.usersFile);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+			userFile.createNewFile();
 			
-			YamlConfiguration permFile = YamlConfiguration.loadConfiguration(Genesis.permissionsFile);
-			permFile.set("users." + this.getName(true) + ".permissions", YamlConfiguration.loadConfiguration(Genesis.config).get("defaultPermissions"));
-			try {
-				permFile.save(Genesis.permissionsFile);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+			getConfig().set("Location.FirstIP", getIPAddress());
+			getConfig().set("Location.LastIP", getIPAddress());
+			getConfig().set("EntityData.UUID", this.getUniqueId().toString());
+			getConfig().set("EntityData.Passwd", "");
+			getConfig().set("Mode.GameMode", Genesis.configFile.getBoolean("UC.Mode.GameMode"));
+			getConfig().set("Mode.Fly", Genesis.configFile.getBoolean("UC.Mode.Fly"));
+			getConfig().set("Move.Speed.Allow", Genesis.configFile.getBoolean("UC.Move.Speed.Allow"));
+			getConfig().set("Move.Speed.Walk", Genesis.configFile.getDouble("UC.Move.Speed.Walk"));
+			getConfig().set("Move.Jumps", 0);
+			getConfig().set("Move.JumpsTMP", 1);
+			getConfig().set("Move.Energy", 0);
+			getConfig().set("Permissions", Genesis.configFile.get("DefaultPermissions"));
 		}
+		
+		getConfig().save(userFile);
 	}
 	
 	public boolean isRegistered() {
-		if (YamlConfiguration.loadConfiguration(Genesis.usersFile).contains(this.getName(true) + ".IP"))
-			return true;
-		else
-			return false;
+		if (!userFile.exists()) return false;
+		else return true;
+	}
+	
+	@Override
+	public boolean getAllowFlight() {
+		return getConfig().getBoolean("Mode.Fly");
+	}
+	
+	@Override
+	@Deprecated
+	public GameMode getGameMode() {
+		return super.getGameMode();
+	}
+	
+	@SuppressWarnings("deprecation")
+	public GameMode getGM() {
+		return GameMode.getByValue(getConfig().getInt("Mode.GameMode"));
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Override
+	public void setGameMode(GameMode gameMode) {
+		super.setGameMode(gameMode);
+		getConfig().set("Mode.GameMode", gameMode.getValue());
+		try {
+			getConfig().save(userFile);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void setAllowFly(boolean value) throws IOException {
+		super.setAllowFlight(value);
+		getConfig().set("Mode.Fly", value);
+		getConfig().save(userFile);
+	}
+	
+	@Override
+	@Deprecated
+	public void setAllowFlight(boolean value) {
+		try {
+			setAllowFly(value);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public YamlConfiguration getConfig() {
@@ -76,36 +109,29 @@ public class User extends PlayerCharacter implements ICommandSource
 	}
 	
 	public boolean isAuthorized(String permission) {
-		YamlConfiguration file = YamlConfiguration.loadConfiguration(Genesis.permissionsFile);
 		permission = permission.toLowerCase();
 		
-		if (file.getList("users." + this.getName(true) + ".permissions").contains(permission)) {
+		if (getConfig().getList("Permissions").contains(permission))
 			return true;
-		}
 		
-		List<String> groupsOfPlayer = new ArrayList<String>(file.getStringList("users." + this.getName().toLowerCase() + ".groups"));
-		for (int i = 0; i < groupsOfPlayer.size(); i++) {
-			if (getGroupPermissions(groupsOfPlayer.get(i)).contains(permission)) {
+		List<String> groupsOfPlayer = new ArrayList<String>(getConfig().getStringList("Groups"));
+		for (int i = 0; i < groupsOfPlayer.size(); i++)
+			if (getGroupPermissions(groupsOfPlayer.get(i)).contains(permission)) 
 				return true;
-			}
-		}
 		
 		permission = permission.replaceAll(".\\*", "");
-		if (permission.replaceAll(".", "").length() +1 <= permission.length()) {
-			for (int i = permission.length()-1; i >= 0; i--) {
-				if (permission.charAt(i) == '.') {
+		if (permission.replaceAll(".", "").length() +1 <= permission.length())
+			for (int i = permission.length()-1; i >= 0; i--)
+				if (permission.charAt(i) == '.')
 					return isAuthorized(permission.substring(0, i+1) + "*");
-				}
-			}
-		}
 		
 		return false;
 	}
 	
 	private ArrayList<String> getGroupPermissions(String group) {
 		YamlConfiguration file = YamlConfiguration.loadConfiguration(Genesis.permissionsFile);
-		List<String> perms = new ArrayList<String>(file.getStringList("groups." + group + ".permissions"));
-		List<String> inher = new ArrayList<String>(file.getStringList("groups." + group + ".inheritance"));
+		List<String> perms = new ArrayList<String>(file.getStringList("Groups." + group + ".Permissions"));
+		List<String> inher = new ArrayList<String>(file.getStringList("Groups." + group + ".Inheritance"));
 		for (int i = 0; i < inher.size(); i++) {
 			List<String> tmp = new ArrayList<String>(getGroupPermissions(inher.get(i).toString()));
 			perms.addAll(tmp);
