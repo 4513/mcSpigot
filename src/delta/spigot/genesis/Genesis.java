15546ac0bdd1fdb48b8573c233030d180d3f8d4e
adyhnat <adyhnat@gmail.com>
2017-02-25 23:13:10 +0100

    Update Genesis.java

--- src/delta/spigot/genesis/Genesis.java
+++ src/delta/spigot/genesis/Genesis.java
@@ -7,6 +7,7 @@ import java.util.logging.Logger;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -15,12 +16,12 @@ import delta.spigot.genesis.command.CommandSource;
 import delta.spigot.genesis.entity.PlayerCharacter;
 import delta.spigot.genesis.event.block.BlockBreakListener;
 import delta.spigot.genesis.event.block.BlockPlaceListener;
-import delta.spigot.genesis.event.user.PlayerJoinListener;
-import delta.spigot.genesis.event.user.PlayerKickListener;
-import delta.spigot.genesis.event.user.PlayerLoginListener;
-import delta.spigot.genesis.event.user.PlayerMoveListener;
-import delta.spigot.genesis.event.user.PlayerPreLoginListener;
-import delta.spigot.genesis.event.user.PlayerQuitListener;
+import delta.spigot.genesis.event.user.UserJoinEvent;
+import delta.spigot.genesis.event.user.UserKickEvent;
+import delta.spigot.genesis.event.user.UserLoginEvent;
+import delta.spigot.genesis.event.user.UserMoveEvent;
+import delta.spigot.genesis.event.user.UserPreLoginEvent;
+import delta.spigot.genesis.event.user.UserQuitEvent;
 
 import static delta.spigot.genesis.log.I18n.tl;
 
@@ -31,9 +32,11 @@ public class Genesis extends JavaPlugin implements GenesisPlugin
 	public final PluginDescriptionFile pdFile = this.getDescription();
 	public GenesisPlugin plugin = this;
 	public static final File pluginFolder = new File("plugins" + File.separator + "Genesis");
+	public static final File config = new File("plugins" + File.separator + "Genesis" + File.separator + "config.yml");
+	public static final YamlConfiguration configFile = YamlConfiguration.loadConfiguration(config);
 	public static final File rbFile = new File("plugins" + File.separator + "Genesis" + File.separator + "rollback.yml");
 	public static final File opFile = new File("plugins" + File.separator + "Genesis" + File.separator + "ops.yml");
-	public static final File usersFile = new File("plugins" + File.separator + "Genesis" + File.separator + "users.yml");
+	public static final File usersFolder = new File("plugins" + File.separator + "Genesis" + File.separator + "users");
 	public static final File permissionsFile = new File("plugins" + File.separator + "Genesis" + File.separator + "permissions.yml");
 	public static List<PlayerCharacter> listOfPlayerCharacter;
 
@@ -43,7 +46,11 @@ public class Genesis extends JavaPlugin implements GenesisPlugin
 		logger.warning("[Genesis] Plugin " + pdFile.getName() + " v" + pdFile.getVersion() + " has been enabled.");
 		getConfig().options().copyDefaults(true);
 		saveConfig();
-		registerFiles();
+		try {
+			registerFiles();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		registerListeners();
 	}
 
@@ -86,37 +93,36 @@ public class Genesis extends JavaPlugin implements GenesisPlugin
 	@Override
 	public void registerListeners()
 	{
-		pm.registerEvents(new PlayerPreLoginListener(this), this);
-		pm.registerEvents(new PlayerLoginListener(this), this);
-		pm.registerEvents(new PlayerJoinListener(this), this);
-		pm.registerEvents(new PlayerQuitListener(this), this);
-		pm.registerEvents(new PlayerKickListener(this), this);
-		pm.registerEvents(new PlayerMoveListener(this), this);
+		pm.registerEvents(new UserPreLoginEvent(this), this);
+		pm.registerEvents(new UserLoginEvent(this), this);
+		pm.registerEvents(new UserJoinEvent(this), this);
+		pm.registerEvents(new UserQuitEvent(this), this);
+		pm.registerEvents(new UserKickEvent(this), this);
+		pm.registerEvents(new UserMoveEvent(this), this);
 		
 		pm.registerEvents(new BlockPlaceListener(this), this);
 		pm.registerEvents(new BlockBreakListener(this), this);
 	}
 
 	@Override
-	public void registerFiles()
+	public void registerFiles() throws IOException
 	{
 		tl("creatingFiles");
-		try {
-			if (!usersFile.exists()) {
-				tl("fileNotExists", "usersFile");
-				usersFile.createNewFile();
-			}
-			if (!permissionsFile.exists()) {
-				tl("fileNotExists", "permissionsFile");
-				permissionsFile.createNewFile();
-			}
-			if (!opFile.exists()) {
-				tl("fileNotExists", "opFile");
-				opFile.createNewFile();
-				
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
+		if (!usersFolder.exists()) {
+			tl("folderNotExists", "usersFile");
+			usersFolder.mkdir();
+		}
+		if (!permissionsFile.exists()) {
+			tl("fileNotExists", "permissionsFile");
+			permissionsFile.createNewFile();
+		}
+		if (!opFile.exists()) {
+			tl("fileNotExists", "opFile");
+			opFile.createNewFile();
+		}
+		if (!rbFile.exists()) {
+			tl("fileNotExists", "rbFile");
+			rbFile.createNewFile();
 		}
 	}
 }
