c5c567ec8683eb14161ecbc7f07bde15f945c13f
4513 <sirmichal.4513@gmail.com>
2017-05-25 14:57:18 +0200

    Add files via upload

--- src/delta/spigot/genesis/event/Listener.java
+++ src/delta/spigot/genesis/event/Listener.java
@@ -1,12 +1,36 @@
 package delta.spigot.genesis.event;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Material;
+
 import delta.spigot.genesis.GenesisPlugin;
 
 public class Listener implements org.bukkit.event.Listener
 {
-	protected GenesisPlugin plugin;
+	protected final GenesisPlugin plugin;
+	private static final List<String> materials = _INVALID_getAllMaterial();
 	
-	public Listener(GenesisPlugin plugin) {
+	public Listener(final GenesisPlugin plugin) {
 		this.plugin = plugin;
+	}
+	
+	/*@Deprecated
+	private static List<String> _INVALID_getAllMaterials; {
+		for (Material material : Material.values())
+			materials.add(material.name());
+	}*/
+
+	@Deprecated
+	public static List<String> _INVALID_getAllMaterial() {
+		List<String> materials = new ArrayList<String>();
+		for (Material material : Material.values())
+			materials.add(material.name());
+		return materials;
+	}
+	
+	public static List<String> getAllMaterials() {
+		return materials;
 	}
 }
