913a6bd98e90eb036d15d01e36f7231803b49afd
4513 <sirmichal.4513@gmail.com>
2017-05-25 15:00:29 +0200

    Add files via upload

--- src/delta/spigot/genesis/event/npc/NPCDeathEvent.java
+++ src/delta/spigot/genesis/event/npc/NPCDeathEvent.java
@@ -1,1 +1,19 @@
-s
+package delta.spigot.genesis.event.npc;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+
+import delta.spigot.genesis.Genesis;
+import delta.spigot.genesis.event.Listener;
+
+public class NPCDeathEvent extends Listener
+{
+	public NPCDeathEvent(Genesis plugin) {
+		super(plugin);
+	}
+
+	@EventHandler (priority = EventPriority.NORMAL)
+	public void onDeathEvent(net.citizensnpcs.api.event.NPCDeathEvent event) {
+		
+	}
+}
