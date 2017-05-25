4827be532c820120ba9808e2bbc144c69ce212da
4513 <sirmichal.4513@gmail.com>
2017-05-25 15:03:22 +0200

    Add files via upload

--- src/delta/spigot/genesis/roles/Role.java
+++ src/delta/spigot/genesis/roles/Role.java
@@ -1,1 +1,20 @@
-s
+package delta.spigot.genesis.roles;
+
+public interface Role
+{
+	@Deprecated
+	public String getString();
+	
+	public String getName();
+	
+	public int getLevel();
+	
+	public void setLevel(int level);
+	
+	public float getXp();
+	
+	public void setXp(float xp);
+	
+	@Deprecated
+	public void reset();
+}
